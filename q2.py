da3a9c21646456f8b6e2bf2a4c5545decdba6131
Qianxi Li <45400887+liqianxi@users.noreply.github.com>
2021-01-11 18:36:10 -0800

    third commit

--- q2.py
+++ q2.py
@@ -1,5 +1,6 @@
 import requests as req
 print(req.__version__)
-request = req.get('https://github.com/liqianxi/404lab1/blob/main/q2.py',auth=('user','pass'))
-print(request.status_code)
+request = req.get('https://raw.github.com/liqianxi/404lab1/main/q2.py')
+print(request.text)
+
 
